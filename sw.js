b607e756fbea0134f154caacf9898ae53536b2b3
1395490241lhj-code <1395490241lhj@gmail.com>
2025-11-04 11:30:09 -0500

    1.0.0

---
+++ sw.js
@@ -0,0 +1,25 @@
+
+const CACHE = 'kitchen-cache-v1'
+const ASSETS = [
+  './', './index.html', './styles.css', './app.js',
+  './manifest.webmanifest', './icons/pwa-192.png', './icons/pwa-512.png',
+  './icons/maskable-192.png', './icons/maskable-512.png', './icons/apple-touch-180.png'
+]
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))
+})
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))
+})
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url)
+  if (ASSETS.some(a => url.pathname.endsWith(a.replace('./','/')))){
+    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)))
+    return
+  }
+  e.respondWith(fetch(e.request).then(res=>{
+    const copy = res.clone()
+    caches.open(CACHE).then(c=>c.put(e.request, copy))
+    return res
+  }).catch(()=>caches.match(e.request)))
+})
